342a8b4bda44acc3276a4dbe9161ba11f5fd787f
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-08-11 21:12:38 +0700

    Create cep2025_2026.sql

---
+++ cep2025_2026.sql
@@ -0,0 +1,96 @@
+--trx nipos
+SELECT
+    DATE_part('year',np.connote__created_at)tahun,
+    DATE_part('month',np.connote__created_at) bulan,
+    case when customer_code is null then 'RB'
+    else (case when  t_salesforce.subdit_id is null or LEN(t_salesforce.subdit_id)<2 then 'EB'
+    else t_salesforce.subdit_id END)
+    end as subdit_id,
+coalesce(t_referensi_kantor.regional::varchar,
+	'TIDAK TERDEFINISI') regional,
+    COUNT(connote__connote_code) AS produksi,
+    SUM(
+    COALESCE(connote__connote_service_price, 0) / 1.011
+    + COALESCE(connote__connote_surcharge_amount, 0) / 1.11
+)+
+    SUM(
+        CASE 
+            WHEN UPPER(customer_code) = 'DAGSHOPEE04120A'
+             AND UPPER(custom_field__cod) != 'NONCOD'
+            THEN COALESCE(t_webhook.good_value, 0) * 0.005
+            ELSE COALESCE(np.custom_field__fee_value, 0)
+        END
+    ) pendapatan_final,
+    sum(coalesce(np.connote__chargeable_weight,0))connote__chargeable_weight
+FROM nipos.nipos np
+LEFT JOIN (
+    SELECT DISTINCT resi, good_value
+    FROM nipos.webhook_marketplace
+    WHERE member_id = 'DAGSHOPEE04120A'
+) t_webhook
+ON np.connote__connote_booking_code = t_webhook.resi
+	--joinkeun ka referensi subdit
+left join
+(
+	select
+		distinct idregpelanggan,
+		subdit_id,nm_perusahaan
+	from
+		referensi.m_pelanggan)t_salesforce
+on
+	coalesce(np.customer_code,'KOSONG') = coalesce(t_salesforce.idregpelanggan,'KOSONG')
+	--JOIN ka referensi_kantor
+left join
+(
+	select
+		*
+	from
+		(
+		select
+			t1.kdnopen,
+			t1.ketnopen,
+			t2.regional,
+			t2.kcu,
+			t2.kc,
+			t1.jenis,
+			row_number() over (partition by t1.kdnopen
+		order by
+			t1.kdnopen) as rn
+		from
+			(
+			select
+				kdnopen,
+				ketnopen,
+				kdkantor,
+				jenis
+			from
+				referensi.refrensikantorbaru
+) t1
+		join (
+			select
+				distinct
+nopend_dirian,
+				kc,
+				kcu,
+				regional
+			from
+				referensi.ref_kcu_kc_2023
+) t2
+on
+			t1.kdkantor = t2.nopend_dirian
+) x
+	where
+		rn = 1
+)t_referensi_kantor
+on
+	np.location_data_created__custom_field__nopen = t_referensi_kantor.kdnopen
+WHERE
+    UPPER(connote__location_name) != 'AGP TESTING LOCATION'
+    AND connote__connote_amount >= 0
+    AND connote__connote_service != 'LNINCOMING'
+    AND UPPER(connote__connote_state) NOT IN ('CANCEL', 'PENDING')
+    and np.connote__created_at >'20250101'
+    and np.connote__created_at <'20260801'
+    and np.connote__connote_service not in ('KRT', 'KBM', 'FFE', 'FF-LKPP')
+GROUP BY 1,2,3,4
+order by 1,2,3,4
